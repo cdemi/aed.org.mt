89c8bc45b27b4248d7ec7ea3d6823ca06e1e742f
Christopher Demicoli <christopher.demicoli@outlook.com>
2016-07-04 14:50:08 +0200

    Renaming

--- src/aed.org.mt/Program.cs
+++ src/aed.org.mt/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace aed.com.mt
+namespace aed.org.mt
 {
     public class Program
     {
--- src/aed.org.mt/Startup.cs
+++ src/aed.org.mt/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace aed.com.mt
+namespace aed.org.mt
 {
     public class Startup
     {
